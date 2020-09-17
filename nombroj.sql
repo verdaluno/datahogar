fab300ab8254a1aee0e5641b6426d1b86364add5
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2020-09-17 15:51:42 -0500

    OK

--- nombroj.sql
+++ nombroj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2020 a las 19:06:23
+-- Tiempo de generación: 17-09-2020 a las 22:59:49
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.2.32
 
